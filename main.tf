dcd8c2baa38d2e0b454d20a781316fe1c4dfa7a4
Alan Ricardo <alan.ricardo.silva1@gmail.com>
2023-03-24 21:07:18 -0300

    trocando variaveis

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
-
 resource "aws_instance" "modeloteste" {
-  ami           = var.ami.default
-  instance_type = var.instance.default
-  count = var.counter.default
+  ami           = ami
+  instance_type = instance
+  count = count
 }
